f73f97e543a5aad6f618240f7ca9e5e32b09d9b9
Haydar Selmi <haydarselmi55@gmail.com>
2021-04-19 11:04:26 +0200

    Ajout de la grille des auteurs

--- public/src/vue/components/AuthorList.vue
+++ public/src/vue/components/AuthorList.vue
@@ -1,2 +1,65 @@
 <template>
+<div>
+  <TeamMessage class="team-message"/>
+  <div class="grid-container centered">
+    <AuthorCard 
+    v-for="i in nOfAuthors" :key="i" 
+    :authors="listOfAuthors"
+    />
+  </div>
+</div>
 </template>
+<script>
+
+import TeamMessage from "./viewmessages/TeamMessage.vue";
+import AuthorCard from "./cards/AuthorCard.vue";
+
+export default {
+    data() {
+        return {
+          nOfAuthors: 8,
+          listOfAuthors: [
+            "auteur 1",
+            "auteur 2",
+            "auteur 3",
+            "auteur 4",
+            "auteur 5",
+            "auteur 6",
+            "auteur 7",
+            "auteur 8",
+          ]
+        }
+    },
+    components: {
+        TeamMessage,
+        AuthorCard
+    }
+};
+</script>
+
+<style scoped>
+.team-message {
+  width: 100vw;
+  height: 100vh;
+}
+
+.centered {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.grid-container {
+  z-index: 99999;
+  max-width: 70vw;
+  width: 100;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 1em 1em;
+  grid-template-areas:
+    ". . . ."
+    ". . . .";
+}
+</style>
---
+++ public/src/vue/components/cards/AuthorCard.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="author">
+    <div class="profile">
+      <p>Clique pour prendre une photo !</p>
+    </div>
+    <select name="author">
+      <option value="" selected disabled>[prénom]</option>
+      <option 
+        v-for="author in authors" :key="author" 
+        value="author">
+        {{ author }} 
+      </option>
+    </select>
+  </div>
+</template>
+<script>
+export default {
+    props: {
+        authors: {
+            Type: Array,
+            required: true
+        }
+    }
+}
+</script>
+<style scoped>
+.author {
+  min-width: 50px;
+  min-height: 50px;
+  padding: 0px;
+}
+
+.author select {
+  all: inherit;
+  height: 10px;
+  width: 100%;
+  line-height: 1.2vw;
+  border-radius: 0px;
+  border: 1px solid black;
+  font-size: 1.2vw;
+  text-align: center;
+  background-color: rgb(240, 240, 240);
+}
+
+.author select option[value=""] {
+  text-align: center;
+  font-weight: lighter;
+} 
+
+.author .profile {
+  margin: 0 auto 1vh auto;
+  width: 80%;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom-left-radius:-50px;
+  border-bottom-right-radius:-50px;
+  filter: drop-shadow(0 0 0.75rem rgba(0, 0, 0, 0.2));
+}
+
+.author .profile:after {
+  content: "";
+  display: block;
+  padding-bottom: 100%;
+}
+
+.author .profile:hover {
+  cursor: pointer;
+}
+
+.author .profile p {
+  text-align: center;
+  padding: 0 1%;
+  font-size: 1.1vw;
+  color: gray;
+}
+</style>
