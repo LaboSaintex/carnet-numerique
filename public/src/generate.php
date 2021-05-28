af2fecc8e14908056af47368562f25362e74ab75
Haydar Selmi <haydarselmi55@gmail.com>
2021-05-28 16:24:10 +0200

    Ajout du logo carnet numérique

--- public/src/generate.php
+++ public/src/generate.php
@@ -10,7 +10,7 @@ $projectPath = $parameters['projectPath'];
 $musicPath = $parameters['musicPath'];
 $fontPath = getcwd() . "/public/fonts/cobol/Cobol-Bold.ttf";
 $logoPath = getcwd() . "/public/images/saintex.jpg";
-
+$carnetLogoPath = getcwd() . "/public/images/CarnetTitre.jpg";
 // Adding descriptions for each clip and fade in and fade out filters
 $clipsToDescribe =  glob("$projectPath/*.webm");
 $clipFrameRate = (int) shell_exec("cd $projectPath && ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate $clipsToDescribe[0]");
@@ -25,16 +25,15 @@ for($i = 1; $i <= count($clipsToDescribe); $i++) {
 array_map('unlink', glob("$projectPath/*desc.txt"));
 
 shell_exec("cd $projectPath && ffmpeg -t 2 -f lavfi -i color=c=black:s=1280x720 -r 30 blank.webm");
-shell_exec("cd $projectPath && ffmpeg -i blank.webm -i $logoPath -filter_complex '[0:v][1:v] overlay=(main_w/2)-(overlay_w/2):(main_h/2)-(overlay_h/2)-150' -pix_fmt yuv420p -c:a copy logo.webm");
+shell_exec("cd $projectPath && ffmpeg -i blank.webm -i $carnetLogoPath -filter_complex '[0:v][1:v] overlay=(main_w/2)-(overlay_w/2):(main_h/2)-(overlay_h/2)' -pix_fmt yuv420p -c:a copy logo.webm");
 
 $workshop = $parameters["workshop_type"];
 $title = $parameters["title"];
-shell_exec("cd $projectPath && ffmpeg -i logo.webm -vf 'drawtext=text=$workshop:fontfile=$fontPath:fontcolor=white:fontsize=46:x=(w-text_w)/2:y=(h-text_h)/2, drawtext=text=$title:fontfile=$fontPath:fontcolor=white:fontsize=46:x=(w-text_w)/2:y=((h-text_h)/2)+lh+5' opening.webm");
+shell_exec("cd $projectPath && ffmpeg -f lavfi -i color=size=1280x720:duration=2:rate=30:color=black -vf 'drawtext=text=$workshop:fontfile=$fontPath:fontcolor=white:fontsize=46:x=(w-text_w)/2:y=(h-text_h)/2, drawtext=text=$title:fontfile=$fontPath:fontcolor=white:fontsize=46:x=(w-text_w)/2:y=((h-text_h)/2)+lh+5' opening.webm");
 unlink("$projectPath/blank.webm");
-unlink("$projectPath/logo.webm");
 
-$videosFile = "file 'opening.webm'\n";
-if($parameters["project_desc"] !== "") {
+$videosFile = "file 'logo.webm'\n";
+$videosFile .= "file 'opening.webm'\n";{
 	file_put_contents("$projectPath/project_desc.txt", $parameters["project_desc"]);
 	shell_exec("cd $projectPath && ffmpeg -f lavfi -i color=size=1280x720:duration=5:rate=30:color=black -vf 'drawtext=fontfile=$fontPath:fontsize=46:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:textfile=project_desc.txt' project_desc.webm");
 	unlink("$projectPath/project_desc.txt");
@@ -58,7 +57,7 @@ if(array_key_exists("participants", $parameters)) {
 array_map('unlink', glob("$projectPath/*webm.txt"));
 file_put_contents("$projectPath/videosFile.txt", $videosFile);
 if($musicPath == "/_musics/") {
-	echo(shell_exec("cd $projectPath && ffmpeg -f concat -safe 0 -i videosFile.txt -c copy output.webm"));
+	echo(shell_exec("cd $projectPath && ffmpeg -f concat -safe 0 -i videosFile.txt -b:v 3000K -b:a 192K output.webm"));
 } else {
-	echo(shell_exec("cd $projectPath && ffmpeg -f concat -safe 0 -i videosFile.txt -c copy assembled.webm && ffmpeg -i assembled.webm -i ../..$musicPath -filter_complex ' [1:0] apad ' -shortest -y output.webm"));
+	echo(shell_exec("cd $projectPath && ffmpeg -f concat -safe 0 -i videosFile.txt -b:v 3000K -b:a 192K assembled.webm && ffmpeg -i assembled.webm -i ../..$musicPath -filter_complex ' [1:0] apad ' -shortest -y -b:v 3000K -b:a 192K assembled.webm output.webm"));
 }
